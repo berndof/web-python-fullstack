5e2715a0bd815c38083b300d48237041de6c85b6
berndof <berndofb@gmail.com>
2025-02-13 03:43:06 -0300

    inject service on route

--- backend/app/api/v1/routes/item.py
+++ backend/app/api/v1/routes/item.py
@@ -1,15 +1,15 @@
 from fastapi import APIRouter, Depends
 
-from app.api.v1.dependencies import item_repository
+from app.api.v1.dependencies import item_service
 
 router = APIRouter(prefix="/item", tags=["item"])
 
 
-@router.get("/get_items")
-async def get_items(repository=Depends(item_repository)):  # type: ignore
-    return await repository.get_items()
+@router.get("/get_all")
+async def get_items(service=Depends(item_service)):  # type: ignore
+    return await service.get_all()
 
 
 @router.get("/create")
-async def create_item(repository=Depends(item_repository)):  # type: ignore
-    return await repository.create()
+async def create_item(service=Depends(item_service)):  # type: ignore
+    return await service.create()
