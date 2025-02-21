54afeeb9e9174d520d43e41235892d9c806f71ad
berndof <berndofb@gmail.com>
2025-02-21 11:34:39 -0300

    frontend stack ready

---
+++ frontend/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+# Monta a pasta /dist para servir os arquivos estáticos (CSS, JS, etc.)
+app.mount("/static", StaticFiles(directory="dist"), name="static")
+
+# Configura o diretório de templates
+templates = Jinja2Templates(directory="src")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse(
+        "index.html", {"request": request, "title": "Meu Projeto Frontend"}
+    )
---
+++ frontend/src/app.js
@@ -0,0 +1,9 @@
+import "franken-ui/js/core.iife";
+import "franken-ui/js/icon.iife";
+
+
+import Alpine from 'alpinejs'
+ 
+window.Alpine = Alpine
+ 
+Alpine.start()
